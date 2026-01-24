c36f6d75857317d69e816b4445686321b678983b
Red691 <kumarnikhil05848@gmail.com>
2026-01-24 21:54:32 +0530

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,6 @@
+# buttons.py
+BTN_LIST = [
+    "https://t.me/Hanime_hentai69_bot?start=BQADAQAD5ggAAkeKiUXzmHA-_Ip-6hYE",
+    "https://t.me/Hanime_hentai69_bot?start=BQADAQAD7QgAAkeKiUXCgQE095X8WxYE",
+    "https://t.me/Hanime_hentai69_bot?start=BQADAQAD-ggAAkeKiUWYqvqRl7PwUxYE"
+]
