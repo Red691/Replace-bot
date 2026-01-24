1cd43ec02d5d3c4c6b22c36e29f7072e0682c9e0
Red691 <kumarnikhil05848@gmail.com>
2026-01-25 00:24:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,3 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, Updat
 
 # Heroku Config Vars
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME")  # optional
-MAX_BUTTONS_PER_ROW = int(os.environ.get("MAX_BUTTONS_PER_ROW", 4))  # default 4
