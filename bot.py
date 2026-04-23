0e4336cfdad3a4fc30984745d49e819a1010cd18
Red691 <kumarnikhil05848@gmail.com>
2026-04-23 20:19:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,22 @@ from telegram.ext import (
     filters
 )
 
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "OK"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
+
 # ===== CONFIG =====
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = list(map(int, os.environ.get("ADMIN_ID", "").split()))
